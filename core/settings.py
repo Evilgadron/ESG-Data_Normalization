500bd9f8f3dcd341b4c8da7c1a1f45d71fa9afa9
samith30 <148701928+Evilgadron@users.noreply.github.com>
2026-05-26 21:06:53 +0530

    Update Django settings for production deployment

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-47isr&6we-e2vz9458rxe-t$@kttgxjr!o30$ef6kk)^c*el2v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
